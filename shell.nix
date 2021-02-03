a2e7a3f67afb8509f6a1f9eb5937b9e759a306b3
Max Gonzih <gonzih@gmail.com>
2021-02-03 17:44:04 -0500

    Oh

--- shell.nix
+++ shell.nix
@@ -2,15 +2,52 @@
 
 (pkgs.buildFHSUserEnv {
   name = "flutter-fhs-env";
+  multiPkgs = pkgs: [
+    # Flutter only use these certificates
+    (pkgs.runCommand "fedoracert" { } ''
+      mkdir -p $out/etc/pki/tls/
+      ln -s ${pkgs.cacert}/etc/ssl/certs $out/etc/pki/tls/certs
+    '')
+    pkgs.zlib
+  ];
   targetPkgs = pkgs: (with pkgs; [
-    gnumake
     bash
+    curl
+    dart
     git
+    unzip
+    which
+    xz
+
+    # flutter test requires this lib
+    libGLU
+
+    # for android emulator
+    alsaLib
+    dbus
+    expat
+    libpulseaudio
+    libuuid
+    libGL
+    nspr
+    nss
+    systemd
+  ]) ++ (with pkgs.xorg; [
+    libX11
+    libxcb
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrender
+    libXtst
   ]);
   profile = ''
     export FLUTTER_SDK="$HOME/opt/flutter"
     export DART_SDK="$FLUTTER_SDK/bin/cache/dart-sdk"
-    export PATH="$PATH":"$HOME/.pub-cache/bin":"$FLUTTER_SDK/bin":"$DART_SDK/bin"
+    export PATH="$PATH":"$FLUTTER_SDK/bin"
   '';
   runScript = "bash";
 }).env
